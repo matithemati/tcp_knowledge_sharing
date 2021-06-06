ec5fd7f425fe630b04da1fba9085461b1b7993db
mateuszfurtak <mateusz.furtak@symfoni.com>
2021-06-06 13:03:39 +0200

    complete starter code

--- README.md
+++ README.md
@@ -9,6 +9,8 @@
 - Make sure you have [Flutter](https://flutter.dev/docs/get-started/install) installed
 - Clone this repo: ```git clone https://github.com/matithemati/tcp_knowledge_sharing```
 - Configure and link your app with Google's Firebase as described [here](https://firebase.flutter.dev/docs/overview)
+- Enable all the necessary sign in methods in Firebase console
+- Start Firestore database in Firebase console (for simplicity, you can choose test version)
 
 ## What's in the code?
 
--- lib/services/auth.service.dart
+++ lib/services/auth.service.dart
@@ -17,70 +17,22 @@ class AuthService {
   }
 
   Future<bool> signInWithEmailAndPassword(String email, String password) async {
-    try {
-      await _auth.signInWithEmailAndPassword(email: email, password: password);
-    } catch (e) {
-      return false;
-    }
     return true;
   }
 
   Future<bool> signUpWithEmailAndPassword(String email, String password) async {
-    try {
-      UserCredential user = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      await addRegisterInfo(UserData(
-          uid: user.user!.uid,
-          email: user.user!.email.toString(),
-          registerDate: Timestamp.now(),
-          isAdmin: false));
-    } catch (e) {
-      return false;
-    }
     return true;
   }
 
   Future anonymousSignIn() async {
-    try {
-      await _auth.signInAnonymously();
-    } catch (e) {
-      print(e);
-    }
   }
 
   Future signInWithGoogle() async {
-    try {
-      GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
-
-      if (googleSignInAccount != null) {
-        GoogleSignInAuthentication googleSignInAuthentication =
-            await googleSignInAccount.authentication;
-
-        AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleSignInAuthentication.accessToken,
-          idToken: googleSignInAuthentication.idToken,
-        );
-
-        await _auth.signInWithCredential(credential);
-      }
-    } catch (e) {
-      print(e);
-    }
   }
 
   Future addRegisterInfo(UserData user) async {
-    try {
-      await _db.collection('users').doc(user.email).set(user.toMap());
-    } catch (e) {
-      print(e);
-    }
   }
 
   Future signOut() async {
-    try {
-      await _auth.signOut();
-    } catch (e) {
-      print(e);
-    }
   }
 }
