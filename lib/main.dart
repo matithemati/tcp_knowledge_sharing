f796f4f04589fe57e965569c0e5e0398ed1f9e71
mateuszfurtak <mateusz.furtak@symfoni.com>
2021-06-06 12:53:01 +0200

    complete app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tcp_knowledge_sharing/screens/loginSelect.screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -17,4 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
